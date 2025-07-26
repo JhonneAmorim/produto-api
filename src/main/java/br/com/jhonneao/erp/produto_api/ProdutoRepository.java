312ef54f30c18af2e4e44b6f2a4141e21462e984
JhonneAmorim <106161488+JhonneAmorim@users.noreply.github.com>
2025-07-26 15:11:31 -0300

    feat(repository): Cria o repositório para a entidade Produto

---
+++ src/main/java/br/com/jhonneao/erp/produto_api/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package br.com.jhonneao.erp.produto_api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
